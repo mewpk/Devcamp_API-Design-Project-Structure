e15732ff4b337364df31d0f2a01d27451ac1bf2d
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-22 22:51:20 +0700

    save

--- homework1/frontend/src/App.js
+++ homework1/frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import Form from "./Components/Form"
 
 function App() {
   return (
--- homework1/frontend/src/Components/Form.js
+++ homework1/frontend/src/Components/Form.js
@@ -1,7 +1,72 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Form() {
+
+
   return (
-    <div>Form</div>
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <td><p >Firstname : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Lastname : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Phone : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Email : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Address : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Subdistrict : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >District : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Province : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Postal code : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >ID card number : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Company Name : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Company Address : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Period : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <td><p >Job title : </p></td>
+            <td><input type="text" /></td>
+          </tr>
+          
+        </tbody>
+      </table>
+
+    </>
   )
 }
